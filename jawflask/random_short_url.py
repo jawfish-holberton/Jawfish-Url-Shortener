0f0097f9b3a3d4b1770b43e986c1265e7f107ef5
Andrew Keil <monkeyninja147@gmail.com>
2018-08-31 18:45:53 +0000

    WiP storage

--- jawflask/random_short_url.py
+++ jawflask/random_short_url.py
@@ -10,7 +10,7 @@ def url_generator():
     chars = string.ascii_uppercase + string.ascii_lowercase
     size = 6
     url_string = ''.join(random.choice(chars) for i in range(size))
-    urlstore.save(sys.argv[1])
+    urlstore.save(sys.argv[1], url_string)
     return (url_string)
 
 
--- jawflask/storage.py
+++ jawflask/storage.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-from random_short_url import url_generator
+import json
 
-class urlstore():
+class Urlstore():
     urldict = {}
     _file_path = 'file.json'
-    def save(self, sourceUrl):
+    def save(self, sourceUrl, rng_string):
         '''
             Serializes __objects attribute to JSON file.
         '''
-        urldict['sourceUrl'] = url_generator()
+        url_dict[rng_string] = sourceUrl
         with open(urlstore._file_path, mode='w', encoding="UTF8") as fd:
             json.dump(self.urldict, fd)
+
+urlstore = Urlstore()
