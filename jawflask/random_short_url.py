a76481f1488bf738ab574b1f2fecc778eede76f4
arturadamian <arturadamian@gmail.com>
2018-08-31 20:04:57 +0000

    give an arg

--- jawflask/random_short_url.py
+++ jawflask/random_short_url.py
@@ -6,11 +6,11 @@ import random
 import sys
 
 
-def url_generator():
+def url_generator(sourceUrl):
     chars = string.ascii_uppercase + string.ascii_lowercase
     size = 6
     rng_string = ''.join(random.choice(chars) for i in range(size))
-    urlstore.urldict[rng_string] = sys.argv[1]
+    urlstore.urldict[rng_string] = sourceUrl
     urlstore.save()
     return (rng_string)
 
