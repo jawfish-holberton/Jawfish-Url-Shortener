f462bf48a1964feb5518c6d01b318fac3364e9c9
Sidney Burkett <sidneyriffic@gmail.com>
2018-08-31 10:50:15 -0700

    Add some functional and placeholder code for routes

--- jawflask/routes.py
+++ jawflask/routes.py
@@ -2,18 +2,27 @@
 
 
 import flask
-from flask import request
+from flask import request, abort
 
 @jawfish.route('/')
 @jawfish.route('/index.html')
 def indexpage():
-    """Dispaly basic index page"""
+    """Display basic index page"""
+    return render_template("index.html")
 
 @jawfish.route('/newurl', methods=["POST"])
 def posturl():
     """POST a url to shorten"""
+    try:
+        sourceurl = requests.json()["sourceurl"]
+    except KeyError:
+        abort(404)
+    shrinkurl(sourceurl)
 
 @jawfish.route('/<short>')
 def shorturl(shortname):
     """Return redirect to long url"""
-    
+    try:
+        return redirect(urlstore.urldict["shortname"], code = 302)
+    except KeyError:
+        abort(404)
