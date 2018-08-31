a4aba4a6fb2a7bfcba149be4351f77c4023084fb
Sidney Burkett <sidneyriffic@gmail.com>
2018-08-31 11:25:03 -0700

    Debug routes and set up for interfacing to frontend

--- jawflask/routes.py
+++ jawflask/routes.py
@@ -2,27 +2,34 @@
 
 
 import flask
-from flask import request, abort
+from flask import request, abort, redirect
+from jawflask import jawfish
+from jawflask.storage import urlstore
 
 @jawfish.route('/')
 @jawfish.route('/index.html')
 def indexpage():
     """Display basic index page"""
+    print("Display index")
     return render_template("index.html")
+
 
 @jawfish.route('/newurl', methods=["POST"])
 def posturl():
     """POST a url to shorten"""
+    print("Post url to shorten")
     try:
-        sourceurl = requests.json()["sourceurl"]
+        sourceurl = request.json()["sourceurl"]
     except KeyError:
         abort(404)
-    shrinkurl(sourceurl)
+    url_generator(sourceurl)
+
 
 @jawfish.route('/<short>')
-def shorturl(shortname):
+def shorturl(short):
     """Return redirect to long url"""
+    print("redirect to long url from: ", short)
     try:
-        return redirect(urlstore.urldict["shortname"], code = 302)
+        return redirect(urlstore.urldict[short], code = 302)
     except KeyError:
         abort(404)
