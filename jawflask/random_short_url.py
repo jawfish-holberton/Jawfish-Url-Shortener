64cc1ba0fd541fe1f7fd10cd892d004aff5565d6
kevanlucc <kevan.lucchini@gmail.com>
2018-08-31 17:58:07 +0000

    added dict

--- jawflask/random_short_url.py
+++ jawflask/random_short_url.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 
+from storage import urlstore
+
 import string
 import random
 
+
 def url_generator(size=6, chars=string.ascii_uppercase + string.ascii_lowercase):
-    return ''.join(random.choice(chars) for i in range(size))
+    
+    url_string = ''.join(random.choice(chars) for i in range(size))
+    return (url_string)
+    for key in urldict:
+        urldict['sourceUrl'] = url_generator()
+            
+
+
+
+
 if __name__ == '__main__':
     print(url_generator())
