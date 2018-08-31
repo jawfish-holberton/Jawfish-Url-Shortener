e5e5a7a10002deb9336c8612d076a75118a67361
kevanlucc <kevan.lucchini@gmail.com>
2018-08-31 18:18:22 +0000

    added json storage method

--- jawflask/storage.py
+++ jawflask/storage.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+from random_short_url import url_generator
 
 class urlstore():
     urldict = {}
+    _file_path = 'file.json'
+    def save(self, sourceUrl):
+        '''
+            Serializes __objects attribute to JSON file.
+        '''
+        urldict['sourceUrl'] = url_generator()
+        with open(urlstore._file_path, mode='w', encoding="UTF8") as fd:
+            json.dump(self.urldict, fd)
